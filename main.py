76d9930ec3d8682b6320887d2a7b7eba641bbc0f
Angith Nair <nairangith1@gmail.com>
2020-07-07 09:54:38 +0530

    Final commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from time import sleep
+
+class InstaBot:
+    def __init__(self, username, pswd):
+        self.driver = webdriver.Chrome(executable_path='E:/Instagram Bot/chromedriver.exe')
+        self.driver.get("https://instagram.com")
+        sleep(3)
+        #username_field
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")\
+            .send_keys(username)
+        #password_field
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")\
+            .send_keys(pswd)
+        #login_button
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]")\
+            .click()
+        sleep(5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button")\
+            .click()
+        sleep(5)
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]")\
+            .click()
+        sleep(8)
+
+    def get_unfollowers(self):
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a")\
+            .click()
+        following = self._get_names()
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a")\
+            .click()
+        followers = self._get_names()
+        not_following_back = [user for user in following if user not in followers]
+        print(not_following_back)
+        
+
+
+    def _get_names(self):
+        sleep(2)
+        sugs = self.driver.find_element_by_xpath('//h4[contains(text(), Suggestions)]')
+        self.driver.execute_script('arguments[0].scrollIntoView()', sugs)
+        sleep(2)
+        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script("""
+                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                return arguments[0].scrollHeight;
+                """, scroll_box)
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        # close button
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[1]/div/div[2]/button/svg/path")\
+            .click()
+        return names
+
+my_bot = InstaBot('angith_nair','16029816A&a.')
+my_bot.get_unfollowers()
+my_bot._get_names()
+
+
